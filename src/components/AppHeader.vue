cd446230d6b054a084856aed07b02563fcd98403
EsterSPR <web.esterspreafico@gmail.com>
2023-01-23 12:14:23 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+    
+  <div class="row">
+    <div class="col-3"></div>
+    <div class="col-6"></div>
+    <div class="col-3"></div>
+  </div>
+
+  <div class="row">
+    <div class="col-12"></div>
+  </div>
+
+</template>
+
+<style lang="scss">
+  
+</style>
